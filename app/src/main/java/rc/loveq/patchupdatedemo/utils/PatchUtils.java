dc363776cb61b315ae03213a2045755a68f83568
liao <664215432@qq.com>
2018-02-16 15:48:25 +0800

    修改版本到1.0

--- app/src/main/java/rc/loveq/patchupdatedemo/utils/PatchUtils.java
+++ app/src/main/java/rc/loveq/patchupdatedemo/utils/PatchUtils.java
@@ -13,8 +13,8 @@ public class PatchUtils {
 
     /**
      * @param oldApkPath 原来的本地安装的apk路径
-     * @param newApkPath 与查分包合并后新的apk路径
-     * @param patchPath  查分包路径，从服务器下载下来
+     * @param newApkPath 与差分包合并后新的apk路径
+     * @param patchPath  差分包路径，从服务器下载下来
      */
     public static native void patch(String oldApkPath, String newApkPath,
                                     String patchPath);
